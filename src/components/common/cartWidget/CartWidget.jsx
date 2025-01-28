a8ea970300bd20b80b793de6127ee6e39b4ce86e
jorgeduje <jorgeduje4@gmail.com>
2025-01-27 22:27:46 -0300

    se implementa estructura de carpetas, repaso de props y state y se trabaja con useEffect

---
+++ src/components/common/cartWidget/CartWidget.jsx
@@ -0,0 +1,11 @@
+import { FaShoppingCart } from "react-icons/fa";
+const CartWidget = () => {
+  return (
+    <div>
+      <h2>2</h2>
+      <FaShoppingCart />
+    </div>
+  );
+};
+
+export default CartWidget;
