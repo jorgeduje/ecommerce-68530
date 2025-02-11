1dc81f25f62f160812465af7cd1fdadd36fab2a2
jorgeduje <jorgeduje4@gmail.com>
2025-02-10 22:15:44 -0300

    se trabaja con rutas

--- src/components/layouts/navbar/Navbar.jsx
+++ src/components/layouts/navbar/Navbar.jsx
@@ -1,19 +1,41 @@
 import CartWidget from "../../common/cartWidget/CartWidget";
+import { Link } from "react-router";
 
 const Navbar = () => {
   return (
-    <div>
-      <img
-        src="https://res.cloudinary.com/dnqfh2chg/image/upload/v1738194670/68530/images_rrtrpk.png"
-        alt=""
-      />
-
-      <ul>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        backgroundColor: "steelblue",
+        padding: "8px",
+        alignItems: "center",
+      }}
+    >
+      <Link to="/">
+        <img
+          src="https://res.cloudinary.com/dnqfh2chg/image/upload/v1738194670/68530/images_rrtrpk.png"
+          alt=""
+          style={{
+            width: "100px",
+          }}
+        />
+      </Link>
+      <ul
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "12px",
+          listStyle: "none",
+        }}
+      >
         <li>Todas</li>
-        <li>Urbanas</li>
-        <li>Deportivas</li>
+        <Link to="/category/urbanas">Urbanas</Link>
+        <Link to="/category/deportivas">Deportivas</Link>
       </ul>
-      <CartWidget />
+      <Link to="/cart" className="">
+        <CartWidget />
+      </Link>
     </div>
   );
 };
